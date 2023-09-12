4364f6be12079d55c28575c8d14babb9e934461f
alan14112003 <sonnn.21it@gmil.com>
2023-09-12 14:22:21 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM openjdk:17-jdk-alpine
+
+# Sao chép các tệp tin và thư mục cần thiết vào hình ảnh Docker
+COPY . /app
+WORKDIR /app
+
+# Biên dịch ứng dụng
+RUN javac Server.java
+
+# Mở cổng mà server của bạn sẽ lắng nghe
+EXPOSE 2301
+
+# Chạy server khi container được khởi chạy
+CMD ["java", "Server"]
---
+++ src/server/ServerParallel.java
@@ -0,0 +1,96 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerParallel {
+	private static final int PORT = 2301;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server is running on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                executorService.execute(clientHandler);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private boolean isStop = false;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        	BufferedReader reader;
+        	DataOutputStream dos;
+			try {
+				reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				dos = new DataOutputStream(clientSocket.getOutputStream());
+			
+        	
+            Timer timer = new Timer();
+            
+            
+            while (true) {
+                String input = reader.readLine();
+                
+                if (input.equals("exit")) {
+                	isStop = true;
+                    break;
+                }
+                if (input.equals("stop")) {
+                    isStop = true;
+                }
+                if (input.equals("run")) {
+					isStop = false;
+				}
+                
+	            TimerTask task = new TimerTask() {
+	                @Override
+	                public void run() {
+                        String time = new Date().toString();
+                        if (!isStop) {
+                        	try {
+                                dos.writeUTF(time);
+                                dos.flush(); // Đảm bảo dữ liệu được gửi đi ngay lập tức
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+						}
+                        
+	                }
+	            };
+	
+	            // Xét interval sau mỗi 1 giây (1000 milliseconds)
+	            timer.scheduleAtFixedRate(task, 0, 1000);
+	            
+            }
+            } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    }
+}
+
